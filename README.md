a085f07fc1b5f3fe28646ac6db711689824f6c70
wkan17012021 <wkan@hotmail.co.uk>
2025-09-18 14:09:07 +0100

    update build config and draft readme

---
+++ README.md
@@ -0,0 +1,9 @@
+# Instructions
+
+1. open up your terminal
+2. clone the repo: git clone https://github.com/wkan17012021/uk-genealogy.git
+3. run npm i
+4. run npm run dev (compiles the sass and starts a local server)
+5. Go to localhost:5173 in your browser (default port)
+
+There's a demo here: https://wkan17012021.github.io/uk-genealogy/
--- vite.config.js
+++ vite.config.js
@@ -1,21 +1,9 @@
 import { defineConfig } from "vite";
 
 export default defineConfig({
+   base: "/", // Set this to your repo name
   build: {
-    // The main entry point for your theme's assets.
-    // Vite will automatically handle all imports from this file.
-    rollupOptions: {
-      input: {
-        main: "/main.js",
-      },
-    },
-    // The directory where Vite will output the bundled and minified files.
     outDir: "dist",
-    // Generates a manifest file to help WordPress enqueue the correct files.
-    manifest: true,
-    // Clears the output directory before building.
     emptyOutDir: true,
-    // Ensures assets are copied to the correct folder
-    assetsDir: "",
   },
 });
